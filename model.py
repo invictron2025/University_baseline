e9407e683e6a0fd010bceb88f6af5e5d40ae8760
invictron2025 <invictronsystems@gmail.com>
2025-02-17 19:44:24 +0530

    updated resnet.py

--- model.py
+++ model.py
@@ -2,33 +2,24 @@ import torch
 import torch.nn as nn
 from torch.nn import init
 from torchvision import models
-from torch.autograd import Variable
-from torch.nn import functional as F
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import ResNet50_Weights
 ######################################################################
 
 def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    # print(classname)
-    if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in') # For old pytorch, you may use kaiming_normal.
-    elif classname.find('Linear') != -1:
+    if isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif isinstance(m, nn.Linear):
         init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
         init.constant_(m.bias.data, 0.0)
-    elif classname.find('BatchNorm1d') != -1:
+    elif isinstance(m, nn.BatchNorm1d):
         init.normal_(m.weight.data, 1.0, 0.02)
         init.constant_(m.bias.data, 0.0)
 
 def weights_init_classifier(m):
-    classname = m.__class__.__name__
-    if classname.find('Linear') != -1:
+    if isinstance(m, nn.Linear):
         init.normal_(m.weight.data, std=0.001)
         init.constant_(m.bias.data, 0.0)
 
-def fix_relu(m):
-    classname = m.__class__.__name__
-    if classname.find('ReLU') != -1:
-        m.inplace=True
 
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
@@ -43,8 +34,7 @@ class ClassBlock(nn.Module):
             num_bottleneck = input_dim
         if bnorm:
             add_block += [nn.BatchNorm1d(num_bottleneck)]
-        # if relu:
-        #     add_block += [nn.LeakyReLU(0.1)]
+        
         if droprate>0:
             add_block += [nn.Dropout(p=droprate)]
         add_block = nn.Sequential(*add_block)
@@ -59,13 +49,9 @@ class ClassBlock(nn.Module):
         self.classifier = classifier
     def forward(self, x):
         x = self.add_block(x)
-        if self.return_f:
-            f = x
-            x = self.classifier(x)
-            return x,f
-        else:
-            x = self.classifier(x)
-            return x
+        
+        x = self.classifier(x)
+        return x
 
 
 
@@ -73,25 +59,17 @@ class ClassBlock(nn.Module):
 
 # Define the ResNet50-based Model
 class ft_net(nn.Module):
-
     def __init__(self, class_num, droprate=0.5, stride=2, init_model=None, pool='avg'):
         super(ft_net, self).__init__()
         model_ft = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-        # avg pooling to global pooling
+
         if stride == 1:
             model_ft.layer4[0].downsample[0].stride = (1,1)
             model_ft.layer4[0].conv2.stride = (1,1)
 
         self.pool = pool
-        
-        model_ft.avgpool2 = nn.AdaptiveAvgPool2d((1,1))
-
-        self.model = model_ft
-
-        if init_model!=None:
-            self.model = init_model.model
-            self.pool = init_model.pool
-            #self.classifier.add_block = init_model.classifier.add_block
+        self.model = model_ft if init_model is None else init_model.model
+        self.model.avgpool2 = nn.AdaptiveAvgPool2d((1,1))
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -102,66 +80,34 @@ class ft_net(nn.Module):
         x = self.model.layer2(x)
         x = self.model.layer3(x)
         x = self.model.layer4(x)
-        if self.pool == 'avg+max':
-            x1 = self.model.avgpool2(x)
-            x2 = self.model.maxpool2(x)
-            x = torch.cat((x1,x2), dim = 1)
-        elif self.pool == 'avg':
-            x = self.model.avgpool2(x)
-        elif self.pool == 'max':
-            x = self.model.maxpool2(x)
-        elif self.pool == 'gem':
-            x = self.model.gem2(x)
+       
+        x = self.model.avgpool2(x)
+      
 
-        x = x.view(x.size(0), x.size(1))
+         
         #x = self.classifier(x)
-        return x
+        return x.squeeze(-1).squeeze(-1)
 
 
 
 
 class three_view_net(nn.Module):
-    def __init__(self, class_num, droprate, stride = 2, pool = 'avg', share_weight = False, VGG16=False, circle=False):
+    def __init__(self, class_num, droprate, stride=2, pool='avg', share_weight=False, circle=False):
         super(three_view_net, self).__init__()
-        
-        self.model_1 =  ft_net(class_num, stride = stride, pool = pool)
-        self.model_2 =  ft_net(class_num, stride = stride, pool = pool)
+        self.model_1 = ft_net(class_num, stride=stride, pool=pool)
+        self.model_2 = ft_net(class_num, stride=stride, pool=pool)
+        self.model_3=self.model_1
+        self.classifier = ClassBlock(2048, class_num, droprate, return_f=circle)
 
-        self.model_3 = self.model_1
-  
-        self.circle = circle
-
-        self.classifier = ClassBlock(2048, class_num, droprate, return_f = circle)
       
 
-    def forward(self, x1, x2, x3, x4 = None): # x4 is extra data
-        if x1 is None:
-            y1 = None
-        else:
-            x1 = self.model_1(x1)
-            x1 = x1.view(x1.size(0), x1.size(1))
-            y1 = self.classifier(x1)
+    def forward(self, x1, x2, x3, x4=None):
+        process = lambda x, model: self.classifier(model(x)) if x is not None else None
 
-        if x2 is None:
-            y2 = None
-        else:
-            x2 = self.model_2(x2)
-            x2 = x2.view(x2.size(0), x2.size(1))
-            y2 = self.classifier(x2)
+        y1 = process(x1, self.model_1)
+        y2 = process(x2, self.model_2)
+        y3 = process(x3, self.model_1)  # model_3 is the same as model_1
+        y4 = process(x4, self.model_2) if x4 is not None else None
 
-        if x3 is None:
-            y3 = None
-        else:
-            x3 = self.model_3(x3)
-            x3 = x3.view(x3.size(0), x3.size(1))
-            y3 = self.classifier(x3)
-
-        if x4 is None:
-            return y1, y2, y3
-        else:
-            x4 = self.model_2(x4)
-            x4 = x4.view(x4.size(0), x4.size(1))
-            y4 = self.classifier(x4)
-            return y1, y2, y3, y4
-
+        return (y1, y2, y3, y4) if x4 is not None else (y1, y2, y3)
 
